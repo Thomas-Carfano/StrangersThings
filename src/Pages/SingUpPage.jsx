d62383aacc08498e73116474cbf8ec7fbb039959
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-08-08 13:44:19 -0700

    fix sign up page

--- src/Pages/SingUpPage.jsx
+++ src/Pages/SingUpPage.jsx
@@ -15,7 +15,6 @@ const BASE_URL = `https://strangers-things.herokuapp.com/api/2306-fsa-et-web-ft-
 
 const SignUp = ({setTokenResponse}) => {
   const navigate = useNavigate();
-  <Link to="./SignUpPage" title="Home" id="login">Sign In</Link>
   const handleSubmit = (event) => {
 
   event.preventDefault();
@@ -55,10 +54,6 @@ const SignUp = ({setTokenResponse}) => {
     </>
   )
   };
-
-
-
-
 
   function Copyright(props) {
     return (
