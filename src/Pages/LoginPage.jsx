fd675180f94504b433b655eb0a82576578d0f6c7
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-08-08 19:55:53 -0700

    add href .

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -125,7 +125,7 @@ const SignIn = ({setTokenResponse}) => {
             </Button>
             <Grid container>
               <Grid item>
-                <Link href="/SignUp" variant="body2">
+                <Link href="./SignUp" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
