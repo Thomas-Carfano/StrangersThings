b8dcbce16944ff8f0f52ca64caf858ede501b5bd
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-08-08 22:28:45 -0700

    sign up page fix

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -4,15 +4,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-// import {Link} from 'react-router-dom'
+// import Link from '@mui/material/Link';
+// import Grid from '@mui/material/Grid';
+import {Link} from 'react-router-dom'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
-import SignUp from './SignUpPage'
 
 const BASE_URL = `https://strangers-things.herokuapp.com/api/2306-fsa-et-web-ft-sf`
 
@@ -125,20 +124,21 @@ const SignIn = ({setTokenResponse}) => {
             >
               Sign In
             </Button>
-            <Grid container>
+            {/* <Grid container>
               <Grid item>
               <h1>Test1A</h1>
 
-                <Link href="./SignUp" element={<SignUp/>} variant="body2">
+                <Link href="./SignUp" variant="body2">
                   {"Don't have an account? Sign UpTEST"}
                 </Link>
               </Grid>
-            </Grid>
+            </Grid> */}
           </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
+    <Link to="../SignUp" title="SignUp" id="login">Sign Up</Link>
     </>
   );
 }
