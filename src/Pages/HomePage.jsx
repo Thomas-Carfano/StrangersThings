17b87099e2eae05342813296594eccce60f55ebd
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-08-08 19:59:02 -0700

    test

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     console.log("homepage")
     return (
         <>
+        <h1>Test</h1>
         <a><img src='https://upload.wikimedia.org/wikipedia/commons/1/16/The_Pirate_Bay_logo.svg'/></a>
         </>
     )
